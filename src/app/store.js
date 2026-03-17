cb498252085701e5214da5ef2864c887a23de26d
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-17 22:10:30 +0430

    core: add store.js

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit"
+import productsReducer from "../features/products/productsSlice"
+import cartReducer from "../features/cart/cartSlice"
+export const store = configureStore({
+    reducer: {
+        products: productsReducer,
+        cart: cartReducer
+    }
+})
