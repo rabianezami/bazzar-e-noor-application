52d1b4cddcb6c54f7b80797783e011abb2245950
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-18 05:36:30 +0430

    fix: import of slices

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/products/productsSlice"
-import cartReducer from "../features/cart/cartSlice"
+import cartReducer from "../features/cart/CartSlice"
+
 export const store = configureStore({
     reducer: {
         products: productsReducer,
