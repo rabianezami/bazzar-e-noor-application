4cee35d24225c4ea5ee56967ce60d524463b2c1b
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-04-04 19:09:14 +0430

    fix cartSlice import name

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/products/productsSlice"
-import cartReducer from "../features/cart/CartSlice"
+import cartReducer from "../features/cart/cartSlice"
 
 export const store = configureStore({
     reducer: {
